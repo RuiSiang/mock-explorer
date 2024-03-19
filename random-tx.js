d36a9688eb6e402700eb415e12ac6250a8c53bd7
RuiSiang <ruisiang2000@gmail.com>
2024-03-19 14:54:22 -0400

    Change transactions.json

--- random-tx.js
+++ random-tx.js
@@ -4,20 +4,20 @@ const generateHex = (length) => crypto.randomBytes(length).toString('hex');
 const generateAddress = () => `addr_${generateHex(3)}`;
 const generateTxId = () => `tx_${generateHex(4)}`;
 const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
-let currentTimestamp = new Date(); 
+let currentTimestamp = new Date();
 
 const generateTimestamp = () => {
-  const newTimestamp = new Date(currentTimestamp.getTime() + getRandomInt(1000, 60000)); 
-  currentTimestamp = newTimestamp; 
+  const newTimestamp = new Date(currentTimestamp.getTime() + getRandomInt(1000, 60000));
+  currentTimestamp = newTimestamp;
   return newTimestamp.toISOString();
 };
 let addressBalances = {};
-let spentOutputs = new Map(); 
+let spentOutputs = new Map();
 
 const createCoinbaseTransaction = () => {
   const address = generateAddress();
   const txid = generateTxId();
-  const amount = 200; 
+  const amount = 200;
   addressBalances[address] = (addressBalances[address] || 0) + amount;
   return {
     txid: txid,
@@ -33,12 +33,14 @@ const createCoinbaseTransaction = () => {
 const selectInputs = (amountNeeded) => {
   const inputs = [];
   let totalSelected = 0;
+  const thresholdMultiplier = 1 + (Math.random() * 1);
+  const targetAmount = amountNeeded * thresholdMultiplier;
 
   for (const [txid, { address, amount }] of spentOutputs) {
-    if (totalSelected >= amountNeeded) break; 
+    if (totalSelected >= targetAmount) break;
     inputs.push({ txid, address });
     totalSelected += amount;
-    spentOutputs.delete(txid); 
+    spentOutputs.delete(txid);
   }
 
   return { inputs, totalSelected };
@@ -46,16 +48,21 @@ const selectInputs = (amountNeeded) => {
 
 const generateTransaction = () => {
   const txid = generateTxId();
-  const amountNeeded = getRandomInt(1, 1000); 
+  const amountNeeded = getRandomInt(1, 100);
   const { inputs, totalSelected } = selectInputs(amountNeeded);
-  if (inputs.length === 0) return null; 
+  if (inputs.length === 0) return null;
 
   const outputs = [];
-  const remainingAmount = totalSelected - amountNeeded; 
-  outputs.push({ address: generateAddress(), amount: amountNeeded }); 
-  if (remainingAmount > 0) {
-    const changeAddress = generateAddress(); 
-    outputs.push({ address: changeAddress, amount: remainingAmount });
+  outputs.push({ address: generateAddress(), amount: amountNeeded });
+
+  let remainingAmount = totalSelected - amountNeeded;
+  const numberOfChangeAddresses = getRandomInt(1, 3);
+  for (let i = 0; i < numberOfChangeAddresses; i++) {
+    const amountForThisAddress = (i === numberOfChangeAddresses - 1) ? remainingAmount : Math.floor(remainingAmount / (numberOfChangeAddresses - i));
+    if (amountForThisAddress > 0) {
+      outputs.push({ address: generateAddress(), amount: amountForThisAddress });
+      remainingAmount -= amountForThisAddress;
+    }
   }
 
   outputs.forEach(output => {
@@ -70,7 +77,7 @@ const generateTransaction = () => {
 const generateTransactions = (numTransactions) => {
   const transactions = [];
   for (let i = 0; i < numTransactions; i++) {
-    if (i % 5 === 0 || i === 0) { 
+    if (i % 20 === 0 || i === 0) {
       const coinbaseTx = createCoinbaseTransaction();
       transactions.push(coinbaseTx);
 
