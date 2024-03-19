3479bed06fab585aa24e0b5331e75fb8df1f83d4
RuiSiang <ruisiang2000@gmail.com>
2024-03-19 13:29:24 -0400

    clean urls

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const transactions = JSON.parse(fs.readFileSync('transactions.json', 'utf-8'));
 app.use(bodyParser());
 
 // Serve static files from the public directory
-app.use(serve(path.join(__dirname, 'public')));
+app.use(serve(path.join(__dirname, 'public'), { cleanUrls: true }));
 
 // Routes
 // Get all transactions
